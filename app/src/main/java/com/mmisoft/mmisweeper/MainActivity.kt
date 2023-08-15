1c3612207b76eddd583b8e03addce91ba0ab77e4
Mircea Martin <mircea1101@gmail.com>
2023-08-16 00:51:08 +0300

    - converted MainActivity to kotlin and removed unused code and imports

--- app/src/main/java/com/mmisoft/mmisweeper/MainActivity.kt
+++ app/src/main/java/com/mmisoft/mmisweeper/MainActivity.kt
@@ -1,98 +1,65 @@
-package com.mmisoft.mmisweeper;
+package com.mmisoft.mmisweeper
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.Handler
+import android.view.KeyEvent
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
 
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.KeyEvent;
-import android.view.WindowManager;
-import android.widget.Toast;
-
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    private static SharedPreferences sharedPreferences;
-    private static SharedPreferences.Editor editor;
-    int count;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            hideSystemBars();
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            hideSystemBars()
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }
     }
 
-    public void changeMainFragment(Fragment fragment, String tag) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-
-        ft.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_left, R.anim.slide_out_right);
-
-        ft.replace(R.id.fragmentContainerView, fragment, tag)
+    fun changeMainFragment(fragment: Fragment?, tag: String?) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_left, R.anim.slide_out_right)
+        ft.replace(R.id.fragmentContainerView, fragment!!, tag)
                 .setReorderingAllowed(true)
                 .addToBackStack(tag)
-                .commit();
+                .commit()
     }
 
-
-
-    public static void set(String key, String value) {
-        editor.putString(key, value);
-        editor.commit();
-    }
-
-    private void hideSystemBars() {
-        WindowInsetsControllerCompat windowInsetsController =
-                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
-        if (windowInsetsController == null) {
-            return;
-        }
+    private fun hideSystemBars() {
+        val windowInsetsController = ViewCompat.getWindowInsetsController(window.decorView)
+                ?: return
         // Configure the behavior of the hidden system bars
-        windowInsetsController.setSystemBarsBehavior(
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        );
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         // Hide both the status bar and the navigation bar
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 
-    public void backBtn(){
-        this.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK));
-        this.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_BACK));
+    fun backBtn() {
+        dispatchKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK))
+        dispatchKeyEvent(KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_BACK))
     }
 
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(getSupportFragmentManager().getBackStackEntryCount() == 0 && keyCode == KeyEvent.KEYCODE_BACK){
-            count++;
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (supportFragmentManager.backStackEntryCount == 0 && keyCode == KeyEvent.KEYCODE_BACK) {
+            count++
             if (count > 1) {
-                moveTaskToBack(true);
+                moveTaskToBack(true)
             } else {
-                Toast.makeText(this, "Press back again to Leave!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Press back again to Leave!", Toast.LENGTH_SHORT).show()
                 // resetting the counter in 2s
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        count = 0;
-                    }
-                }, 2000);
-                return false;
+                val handler = Handler()
+                handler.postDelayed({ count = 0 }, 2000)
+                return false
             }
         }
-        return super.onKeyDown(keyCode, event);
+        return super.onKeyDown(keyCode, event)
     }
 }
