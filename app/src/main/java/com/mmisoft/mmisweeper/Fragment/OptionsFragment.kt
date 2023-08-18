67be5d7fec1d8b2fe5637fc61a51ae08c57784ae
Mircea Martin <mircea1101@gmail.com>
2023-08-18 19:16:55 +0300

    - converted OptionsFragment to kotlin

--- app/src/main/java/com/mmisoft/mmisweeper/Fragment/OptionsFragment.kt
+++ app/src/main/java/com/mmisoft/mmisweeper/Fragment/OptionsFragment.kt
@@ -1,127 +1,89 @@
-package com.mmisoft.mmisweeper.Fragment;
+package com.mmisoft.mmisweeper.Fragment
 
-import static android.content.Context.MODE_PRIVATE;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
-
-import com.mmisoft.mmisweeper.MainActivity;
-import com.mmisoft.mmisweeper.R;
-
-import java.util.Arrays;
+import android.app.AlertDialog
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.mmisoft.mmisweeper.MainActivity
+import com.mmisoft.mmisweeper.R
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link OptionsFragment#newInstance} factory method to
+ * A simple [Fragment] subclass.
+ * Use the [OptionsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-public class OptionsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
+class OptionsFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OptionsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OptionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OptionsFragment newInstance(String param1, String param2) {
-        OptionsFragment fragment = new OptionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private var mParam1: String? = null
+    private var mParam2: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            mParam1 = requireArguments().getString(ARG_PARAM1)
+            mParam2 = requireArguments().getString(ARG_PARAM2)
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_options, container, false);
-
-        ImageButton backBtn = v.findViewById(R.id.backOptionsButton);
-        Button themeBtn = v.findViewById(R.id.themeBtn);
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MainActivity)getActivity()).backBtn();
-            }
-        });
-
-        themeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences sharedPreferences = getContext().getSharedPreferences("Theme",MODE_PRIVATE);
-                SharedPreferences.Editor myEdit = sharedPreferences.edit();
-
-                String[] options = {"default", "minecraft iron", "minecraft gold", "minecraft diamond"};
-                int checkedItem = 0;
-                String selectedOption = sharedPreferences.getString("theme", "default");
-                for(int i = 0; i < options.length; i++){
-                    if(options[i].equals(selectedOption)){
-                        checkedItem = i;
-                    }
+        val v = inflater.inflate(R.layout.fragment_options, container, false)
+        val backBtn = v.findViewById<ImageButton>(R.id.backOptionsButton)
+        val themeBtn = v.findViewById<Button>(R.id.themeBtn)
+        backBtn.setOnClickListener { (activity as MainActivity?)!!.backBtn() }
+        themeBtn.setOnClickListener {
+            val sharedPreferences = requireContext().getSharedPreferences("Theme", Context.MODE_PRIVATE)
+            val myEdit = sharedPreferences.edit()
+            val options = arrayOf("default", "minecraft iron", "minecraft gold", "minecraft diamond")
+            var checkedItem = 0
+            val selectedOption = sharedPreferences.getString("theme", "default")
+            for (i in options.indices) {
+                if (options[i] == selectedOption) {
+                    checkedItem = i
                 }
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle("Pick A Theme");
-                builder.setCancelable(false);
-                builder.setSingleChoiceItems(options, checkedItem, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        myEdit.putString("theme", options[i]);
-                    }
-                });
-
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                myEdit.apply();
-                                Toast.makeText(getContext(), sharedPreferences.getString("theme", "default") + " theme is selected", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                builder.setNegativeButton("Cancel", null);
-                builder.show();
             }
-        });
-        return v;
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Pick A Theme")
+            builder.setCancelable(false)
+            builder.setSingleChoiceItems(options, checkedItem) { dialogInterface, i -> myEdit.putString("theme", options[i]) }
+            builder.setPositiveButton("OK") { dialogInterface, i ->
+                myEdit.apply()
+                Toast.makeText(context, sharedPreferences.getString("theme", "default") + " theme is selected", Toast.LENGTH_SHORT).show()
+            }
+            builder.setNegativeButton("Cancel", null)
+            builder.show()
+        }
+        return v
     }
 
+    companion object {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment OptionsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(param1: String?, param2: String?): OptionsFragment {
+            val fragment = OptionsFragment()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
 }
