df9691ba65d6bb9a75ce98d7bdff4f8d1a6b5299
MirchM <mircea1101@gmail.com>
2023-08-21 23:31:30 +0300

    - converted Cell from java to kotlin

--- app/src/main/java/com/mmisoft/mmisweeper/Game/Cell.kt
+++ app/src/main/java/com/mmisoft/mmisweeper/Game/Cell.kt
@@ -1,46 +1,21 @@
-package com.mmisoft.mmisweeper.Game;
+package com.mmisoft.mmisweeper.Game
 
-import java.io.Serializable;
+import java.io.Serializable
 
-public class Cell implements Serializable {
-    public static final int BOMB = -1;
-    public static final int BLANK = 0;
-    private boolean isFlagged, isRevealed;
-    private int value;
-    private int id;
+class Cell(val id: Int) : Serializable {
+    var isFlagged = false
+        private set
+    @JvmField
+    var isRevealed = false
+    @JvmField
+    var value: Int = BLANK
 
-    public Cell(int id){
-        this.id = id;
-        isFlagged = false;
-        isRevealed = false;
-        value = Cell.BLANK;
+    fun toggleFlagged() {
+        isFlagged = !isFlagged
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public boolean isFlagged() {
-        return isFlagged;
-    }
-
-    public void toggleFlagged() {
-        isFlagged = !isFlagged;
-    }
-
-    public boolean isRevealed() {
-        return isRevealed;
-    }
-
-    public void setRevealed(boolean clicked) {
-        isRevealed = clicked;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
+    companion object {
+        const val BOMB = -1
+        const val BLANK = 0
     }
 }
